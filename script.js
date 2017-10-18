e99dac619de8ba1269752be90e8ea6d129158edd
Mike Holzbach <maholzbach@gmail.com>
2017-10-18 16:21:18 -0400

    More functionality added

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
 });
+
+$(document).ready(function() {
+  $(".modal").modal();
+});
+
+$("#shop").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: $("#shop-content").offset().top
+    },
+    1000
+  );
+});
+
+$("#about").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: $("#about-content").offset().top
+    },
+    1000
+  );
+});
+
+let cartItems = 0;
+let cartTotalPrice = 0;
+
+const onClick = value => {
+  const targetValue = parseInt(value);
+  cartItems += 1;
+  document.getElementById("cart-total").innerHTML = cartItems;
+  cartTotalPrice += targetValue;
+  document.getElementById("cart-total-price").innerHTML = `$ ${cartTotalPrice}`;
+};

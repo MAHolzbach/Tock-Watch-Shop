1ac1be10af1595d82061a8a7db1a22a255a6d119
Mike <maholzbach@gmail.com>
2017-10-23 12:24:33 -0400

    Scrolling added for contact form menu item

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-$(document).ready(function() {
-  $(".button-collapse").sideNav();
+$( document ).ready(function(){
+	$(".button-collapse").sideNav()
 });
 
 $(document).ready(function() {
-  $(".modal").modal();
+	$('.modal').modal();
 });
 
 $("#shop").click(function() {
-  $("html, body").animate(
-    {
-      scrollTop: $("#shop-content").offset().top
-    },
-    1000
-  );
+	$("html, body").animate({
+		scrollTop: $("#shop-content").offset().top}, 1000);
 });
 
 $("#about").click(function() {
-  $("html, body").animate(
-    {
-      scrollTop: $("#about-content").offset().top
-    },
-    1000
-  );
+	$("html, body").animate({
+		scrollTop: $("#about-content").offset().top}, 1000);
+});
+
+$("#contact").click(function() {
+	$("html, body").animate({
+		scrollTop: $("#contact-form").offset().top}, 1000);
+});
+
+$("#shop2").click(function() {
+	$("html, body").animate({
+		scrollTop: $("#shop-content").offset().top}, 1000);
+});
+
+$("#about2").click(function() {
+	$("html, body").animate({
+		scrollTop: $("#about-content").offset().top}, 1000);
+});
+
+$("#contact2").click(function() {
+	$("html, body").animate({
+		scrollTop: $("#contact-form").offset().top}, 1000);
 });
 
 let cartItems = 0;
 let cartTotalPrice = 0;
 
-const onClick = value => {
-  const targetValue = parseInt(value);
-  cartItems += 1;
-  document.getElementById("cart-total").innerHTML = cartItems;
-  cartTotalPrice += targetValue;
-  document.getElementById("cart-total-price").innerHTML = `$ ${cartTotalPrice}`;
+const onClick = (value) => {
+	const targetValue = parseInt(value);
+	cartItems += 1;
+	document.getElementById("cart-total").innerHTML = cartItems;
+	document.getElementById("cart-total2").innerHTML = cartItems;
+	cartTotalPrice += targetValue;
+	document.getElementById("cart-total-price").innerHTML = `$ ${cartTotalPrice}`;
 };

fdb15cf461bf710900b5ba90e46b27c66a431663
Mike <maholzbach@gmail.com>
2017-11-30 11:32:51 -0500

    working on cart modal

--- script.js
+++ script.js
@@ -36,12 +36,50 @@ $("#contact2").click(function() {
 		scrollTop: $("#contact-form").offset().top}, 1000);
 });
 
-let cartItems = 0;
-let cartTotalPrice = 0;
+	let cartItems = 0;
+	let cartTotalPrice = 0;
+	const cartInventory = [
+	{
+		name: "Watch 1",
+		totalInCart: 0	
+	},
+		{
+		name: "Watch 2",
+		totalInCart: 0	
+	},
+	{
+		name: "Watch 3",
+		totalInCart: 0	
+	},
+	{
+		name: "Watch 4",
+		totalInCart: 0	
+	},
+	{
+		name: "Watch 5",
+		totalInCart: 0	
+	},
+	{
+		name: "Watch 6",
+		totalInCart: 0	
+	},
+	{
+		name: "Watch 7",
+		totalInCart: 0	
+	},
+	{
+		name: "Watch 8",
+		totalInCart: 0	
+	}
+];
 
-const onClick = (value) => {
+const onClick = (value, watchName) => {
 	const targetValue = parseInt(value);
 	cartItems += 1;
+	let newCartInventory = cartInventory.map(watch => {
+		if(watchName === watch.name) {watch.totalInCart +=1};
+		console.log(watch.totalInCart);	
+	})
 	document.getElementById("cart-total").innerHTML = cartItems;
 	document.getElementById("cart-total2").innerHTML = cartItems;
 	cartTotalPrice += targetValue;
